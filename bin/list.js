0a4111176d4dd6bdc6e2fc3c17d3a0589a452d3d
travis <chris@ethereum.org>
2017-02-03 00:54:00 +0000

    Added compiler version v0.4.10-nightly.2017.2.3+commit.5ce79609

--- bin/list.js
+++ bin/list.js
@@ -1,5 +1,6 @@
 
 var soljsonSources = [
+  "soljson-v0.4.10-nightly.2017.2.3+commit.5ce79609.js",
   "soljson-v0.4.10-nightly.2017.2.2+commit.8f9839c6.js",
   "soljson-v0.4.10-nightly.2017.2.1+commit.c1a675da.js",
   "soljson-v0.4.10-nightly.2017.1.31+commit.747db75a.js",
@@ -86,11 +87,11 @@ var soljsonSources = [
   "soljson-v0.4.3-nightly.2016.10.10+commit.119bd4ad.js",
   "soljson-v0.4.3-nightly.2016.9.30+commit.d5cfb17b.js",
   "soljson-v0.4.2+commit.af6afb04.js",
-  "soljson-v0.4.2-nightly.2016.9.17+commit.212e0160.js",
-  "soljson-v0.4.2-nightly.2016.9.17+commit.bc8476a.js",
-  "soljson-v0.4.2-nightly.2016.9.17+commit.a78e7794.js",
   "soljson-v0.4.2-nightly.2016.9.17+commit.62f13ad8.js",
   "soljson-v0.4.2-nightly.2016.9.17+commit.60f432e8.js",
+  "soljson-v0.4.2-nightly.2016.9.17+commit.212e0160.js",
+  "soljson-v0.4.2-nightly.2016.9.17+commit.a78e7794.js",
+  "soljson-v0.4.2-nightly.2016.9.17+commit.bc8476a.js",
   "soljson-v0.4.2-nightly.2016.9.15+commit.8a4f8c2.js",
   "soljson-v0.4.2-nightly.2016.9.15+commit.6a80511.js",
   "soljson-v0.4.2-nightly.2016.9.13+commit.2bee7e9.js",

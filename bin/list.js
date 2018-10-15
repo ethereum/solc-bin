868d773428b6bb13ce45c99647f9ab79ede10f68
travis <chris@ethereum.org>
2018-10-15 10:07:52 +0000

    Added compiler version v0.5.0-nightly.2018.10.15+commit.b965fd6e

--- bin/list.js
+++ bin/list.js
@@ -1,5 +1,6 @@
 
 var soljsonSources = [
+  "soljson-v0.5.0-nightly.2018.10.15+commit.b965fd6e.js",
   "soljson-v0.5.0-nightly.2018.10.12+commit.1d312c8e.js",
   "soljson-v0.5.0-nightly.2018.10.11+commit.6b5d041e.js",
   "soljson-v0.5.0-nightly.2018.10.10+commit.6200b4b.js",

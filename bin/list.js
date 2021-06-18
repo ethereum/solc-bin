78ba4c0be643866db87ff8ab56d31f3a06e72c05
emscripten nightly action <builds@ethereum.org>
2021-06-18 01:22:36 +0000

    Nightly build v0.8.6-nightly.2021.6.17+commit.11281586

--- bin/list.js
+++ bin/list.js
@@ -1,5 +1,6 @@
 
 var soljsonSources = [
+  "soljson-v0.8.6-nightly.2021.6.17+commit.11281586.js",
   "soljson-v0.8.6-nightly.2021.6.16+commit.61468301.js",
   "soljson-v0.8.6-nightly.2021.6.15+commit.e7bf1cc7.js",
   "soljson-v0.8.6-nightly.2021.6.14+commit.b2ffa910.js",
--- bin/soljson-nightly.js
+++ bin/soljson-nightly.js
@@ -1,1 +1,1 @@
-soljson-v0.8.6-nightly.2021.6.16+commit.61468301.js
+soljson-v0.8.6-nightly.2021.6.17+commit.11281586.js
